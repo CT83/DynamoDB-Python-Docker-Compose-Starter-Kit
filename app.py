bf55581d2b0bbc18e16f4c2bb8589bc036449d8a
Rohan Sawant <rohansawantct83@gmail.com>
2019-10-29 14:46:45 +0530

    Added db operations

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-import time
-
 from pynamodb.attributes import (
-    UnicodeAttribute
-)
+    UnicodeAttribute,
+    NumberAttribute)
 from pynamodb.models import Model
 
 
@@ -11,14 +9,32 @@ class User(Model):
         table_name = 'User'
         host = "http://database:8000"
 
-    full_name = UnicodeAttribute(hash_key=True)
+    id = NumberAttribute(hash_key=True)
+    name = UnicodeAttribute()
+
+    def __repr__(self):
+        return "<User {} name:{}>".format(self.id, self.name)
 
 
 if __name__ == '__main__':
     if not User.exists():
         print("User Table does not exist, creating one...")
-        User.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
     else:
-        print("User Table already exists. Nice!")
+        print("User Table already exists.")
+        print("Deleting and recreating the User Table...")
+        User.delete_table()
+    User.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
+
+    print("Adding items to the table...")
+    user = User(id=0, name="Rohan")
+    user.save()
+
+    print("Selecting users from User Table...")
+    stored_user = User.get(0)
+    print(stored_user)
+
+    print("Scanning users from User Table...")
+    for user in User.scan(User.name == "Rohan"):
+        print(user)
 
     print("Exiting...")
