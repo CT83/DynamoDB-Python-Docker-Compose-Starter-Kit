4461e1188734f1c77a1d83011bfc1976fbcf8a0b
Rohan Sawant <rohansawantct83@gmail.com>
2019-11-04 16:48:07 +0530

    Cleaned up code

--- app.py
+++ app.py
@@ -20,12 +20,8 @@ class User(Model):
 
 @hydra.main(config_path='env-config.yml')
 def main(cfg):
-    load_dotenv() # Load env. vars
-    print("Running main..")
-    if cfg.db.service == 'docker':
-        db_path = cfg.get('db').get('path') or None
-        User.Meta.host = db_path
-    print("Connecting to {}...".format(User.Meta.host))
+    load_dotenv()  # Load env. vars
+    set_config(cfg)
 
     if not User.exists():
         print("User Table does not exist, creating one...")
@@ -34,16 +30,28 @@ def main(cfg):
         print("Deleting and recreating the User Table...")
         User.delete_table()
     User.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
+
     print("Adding user to the table...")
     user = User(id=0, name="Rohan")
     user.save()
+
     print("Selecting users from User Table...")
     stored_user = User.get(0)
     print(stored_user)
+
     print("Scanning users from User Table...")
     for user in User.scan(User.name == "Rohan"):
         print(user)
     print("Exiting...")
+
+
+def set_config(cfg):
+    """Picks up the settings from the config files and automatically decides between using Local vs AWS DynamoDB"""
+    if cfg.db.service == 'docker':
+        print("Running with Local DynamoDB...")
+        db_path = cfg.get('db').get('path') or None
+        User.Meta.host = db_path
+    print("Running with AWS DynamoDB using the credentials in the .env file...")
 
 
 if __name__ == "__main__":
